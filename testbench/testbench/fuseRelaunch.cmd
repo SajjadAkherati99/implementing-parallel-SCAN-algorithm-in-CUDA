79159d81ad06fd5f7fe2e0681af1085fb1323f0c
sajjadakherati-svg <73754256+sajjadakherati-svg@users.noreply.github.com>
2021-09-15 17:29:25 +0430

    Block Matrix Multiplication In Parallel

--- testbench/testbench/fuseRelaunch.cmd
+++
@@ -1,1 +0,0 @@
--intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -lib "secureip" -o "D:/sajjad/course/electrical engineering/digital/project/testbench/testbench/crccodetb_isim_beh.exe" -prj "D:/sajjad/course/electrical engineering/digital/project/testbench/testbench/crccodetb_beh.prj" "work.crccodetb" "work.glbl" 
